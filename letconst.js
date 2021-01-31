2ae54d99fde6a8e0c1ad8a434cc5ce595584e31b
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-01 01:40:13 +0600

    ES6 code continue

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const bou = "jantoos";
+// bou1 = "fantoos";
+console.log(bou);
+
+const numbers = [23,343];
+numbers[1] = 88;
+numbers.push(22);
+console.log(numbers);
+
+
+let patientName = 'Rahim Chacha';
+patientName = "chachi";
+console.log(patientName);
+
+let sum = 0;
+for(var i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
