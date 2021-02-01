784ddea340ab30c6838160de4dcab8eaa0d97694
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-02 01:38:40 +0600

    destructing assignment code practice

--- destructure.js
+++ destructure.js
@@ -1,2 +1,14 @@
 const person = {name: 'khan', age: 25, job: 'programmer', gfName: 'jantoos', address: 'katabon', friendList: ['kala','bala','dala','rala']};
-console.log(person.gfName)
+
+/* const  gfName = person.gfName;
+const address = person.address;
+const job = person.job; */
+const {name,job,address} = person;
+console.log(name,job);
+/* console.log(address);
+console.log(job); */
+
+//destructing assignment 
+let a, b, rest;
+[a, b, ...rest] = [10,29,38,73,723];
+console.log(rest);
