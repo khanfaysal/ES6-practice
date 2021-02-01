3e010a5a07040695f7dd6675c63c4cccfb1823c1
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-01 11:25:41 +0600

    arrow js code running

--- default.js
+++ default.js
@@ -1,4 +1,10 @@
-function add(num1, num2){
+
+// backup function parameter value
+function add(num1, num2 = 29){
+    // 
+    // num2 = num2 || 20;
     return num1 + num2;
 }
-var total = add(5,5);
+const total = add(5, 5);
+console.log('your total number is :', total);
+
---
+++ template.js
@@ -0,0 +1,12 @@
+// template string and multiple line string
+
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+const fullName = firstName +' '+ lastName +' '+ 'is a good boy';
+const fullName2 = `${firstName} ${20+20} is a good boy`
+console.log(fullName2);
+
+const multilineString = `I love you 
+I miss you 
+I hug you`;
+console.log(multilineString);
